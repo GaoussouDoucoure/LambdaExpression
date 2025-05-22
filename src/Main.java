f9d24f71b472b2606a282d8d10c93b549496c8ba
Gaoussou Doucoure <gaousd4@gmail.com>
2025-05-22 02:43:18 -0700

    Lambda Expression with explanations

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+LAMBDA only works with Functional Interface/SAM type
+which are interface with only one method
+ */
+
+@FunctionalInterface
+interface A
+{
+    //	void show();
+    void show(int i);
+//	void show(int i,int j);
+
+
+}
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //   	A obj=() -> System.out.println("in Show");
+        //   	obj.show();
+
+//    	A obj=new A()
+//    	{
+//    		public void show(int i)
+//    		{
+//    			System.out.println("in show"+i);
+//    		}
+//    	};
+//    	obj.show(5);
+
+//    	A obj=(int i) ->System.out.println("in show "+i);
+//    	obj.show(5);
+
+//    	A obj=(int i,int j) ->System.out.println("in show "+i);
+//    	obj.show(5,8);
+
+        A obj= i -> System.out.println("in show "+i);
+        obj.show(5);
+
+        /*
+        we only need curly brackets after the lambda expression ->, if there is more than one statement
+        like above we don't need it because we only have SOUT as statement
+        Also we don't need to say the data type of i after obj creation, its optional, and we don't need
+        parentheses as well since we are dealing with only one parameter and not saying its type too
+         */
     }
 }
